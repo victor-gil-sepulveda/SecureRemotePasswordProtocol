92590ff09590110f6785477df10395805f75f513
Victor Gil <victor.gil.sepulveda@gmail.com>
2016-12-02 13:10:19 +0100

    Added verbose tools

---
+++ C++/SRPTests/src/TestTools.h
@@ -0,0 +1,20 @@
+/*
+ * TestTools.h
+ *
+ *  Created on: Dec 2, 2016
+ *      Author: victor
+ */
+
+#ifndef TESTTOOLS_H_
+#define TESTTOOLS_H_
+
+#define VERBOSE_TESTS
+
+#ifdef VERBOSE_TESTS
+	#define IF_VERBOSE(x) (x)
+#else
+	#define IF_VERBOSE(x)
+#endif
+
+
+#endif /* TESTTOOLS_H_ */
