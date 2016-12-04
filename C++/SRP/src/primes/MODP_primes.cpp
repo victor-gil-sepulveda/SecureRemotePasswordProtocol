bb6a9c19c4242ef1ff1dc8e64f4fcacdc0330d2a
Victor Gil <victor.gil.sepulveda@gmail.com>
2016-12-04 20:46:19 +0100

    Added fucntion to generate big primes

---
+++ C++/SRP/src/primes/MODP_primes.cpp
@@ -0,0 +1,31 @@
+/*
+ * MODP_primes.cpp
+ *
+ *  Created on: Dec 4, 2016
+ *      Author: victor
+ */
+
+#include "MODP_primes.h"
+
+#include <string>
+#include <gmp.h>
+#include <stdexcept>
+
+using std::runtime_error;
+
+void primes::generate_prime(const std::string& prime_str){
+	// TODO: Check build options as a initial way to speed up the exe
+	// https://gmplib.org/manual/Build-Options.html#Build-Options
+
+	mpz_t prime;
+	int op_result =  mpz_init_set_str (
+			prime, // the guy
+			prime_str.c_str(), //string
+			16); // base
+
+	if (op_result != 0){
+		throw runtime_error("ERROR primes::generate_prime: The number could not be generated.");
+	}
+
+	return prime;
+}
---
+++ C++/SRP/src/primes/MODP_primes.h
@@ -0,0 +1,49 @@
+/*
+ * MODP_primes.h
+ *
+ *  Created on: Dec 4, 2016
+ *      Author: victor
+ */
+
+#ifndef PRIMES_MODP_PRIMES_H_
+#define PRIMES_MODP_PRIMES_H_
+#include <string>
+
+
+
+namespace primes{
+
+	mpz_t generate_prime(const std::string& prime_str);
+
+	// From: https://tools.ietf.org/html/rfc3526
+
+	// 1536-bit MODP Group
+	// 2^1536 - 2^1472 - 1 + 2^64 * { [2^1406 pi] + 741804 }
+	const std::string modp_1536 =
+		"FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1"
+		"29024E088A67CC74020BBEA63B139B22514A08798E3404DD"
+		"EF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245"
+		"E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7ED"
+		"EE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3D"
+		"C2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F"
+		"83655D23DCA3AD961C62F356208552BB9ED529077096966D"
+		"670C354E4ABC9804F1746C08CA237327FFFFFFFFFFFFFFFF";
+
+
+	// 2048-bit MODP Group
+	// 2^2048 - 2^1984 - 1 + 2^64 * { [2^1918 pi] + 124476 }
+
+	// 3072-bit MODP Group
+	// 2^3072 - 2^3008 - 1 + 2^64 * { [2^2942 pi] + 1690314 }
+
+	// 4096-bit MODP Group
+	// 2^4096 - 2^4032 - 1 + 2^64 * { [2^3966 pi] + 240904 }
+
+	// 6144-bit MODP Group
+	// 2^6144 - 2^6080 - 1 + 2^64 * { [2^6014 pi] + 929484 }
+
+	// 8192-bit MODP Group
+	// 2^8192 - 2^8128 - 1 + 2^64 * { [2^8062 pi] + 4743158 }
+}
+
+#endif /* PRIMES_MODP_PRIMES_H_ */
